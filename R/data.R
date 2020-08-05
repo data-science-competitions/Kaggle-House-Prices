29a61036aa110d5514e19fc16564815c3c2d1b46
harell <harel@loostiger.com>
2020-08-05 16:00:45 +1200

    add data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @title Training Set for House Prices Kaggle Competition
+#'
+#' A dataset with more than 150 identifiable house attributes and 1,460
+#' observations.
+#'
+#' @source \url{https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data}
+"train_set"
+
+#' @title Test Set for House Prices Kaggle Competition
+#'
+#' A dataset with more than 150 identifiable house attributes and 1,459
+#' observations.
+#'
+#' @source \url{https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data}
+"test_set"
