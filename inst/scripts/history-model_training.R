a9262358b5f0f8ce82113353ff35f5b94cb1205b
harell <harel@loostiger.com>
2020-08-11 20:51:56 +1200

    add_metaflow_functions

--- inst/scripts/history-model_training.R
+++ inst/scripts/history-model_training.R
@@ -1,37 +1,38 @@
-add_fct <- purrr::partial(golem::add_fct, open = FALSE)
+pkgload::load_all(export_all = FALSE, helpers = FALSE)
+add_function <- purrr::partial(golem::add_fct, open = FALSE)
 
 
 # configure session -------------------------------------------------------
-add_fct("load_package", "ns")
-add_fct("create_experiment", "ns")
+# add_function("load_package", "ns")
+add_function("create_experiment", "ns")
 # session configured ------------------------------------------------------
 
 
 # acquire data ------------------------------------------------------------
-add_fct("ingest_data", "dao")
-add_fct("prepare_data", "dao")
-add_fct("store_data", "dao")
+add_function("ingest_data", "dao")
+add_function("prepare_data", "dao")
+add_function("store_data", "dao")
 # data acquired -----------------------------------------------------------
 
 
 # sample data -------------------------------------------------------------
-add_fct("extract_data", "smp")
-add_fct("split_data", "smp")
+add_function("extract_data", "smp")
+add_function("split_data", "smp")
 # data sampled ------------------------------------------------------------
 
 
 # fit model ---------------------------------------------------------------
-add_fct("compose_formula", "mdl")
-add_fct("log_params", "mdl")
-add_fct("train_model", "mdl")
-add_fct("save_model", "mdl")
+add_function("compose_formula", "mdl")
+add_function("log_params", "mdl")
+add_function("train_model", "mdl")
+add_function("save_model", "mdl")
 # model fitted ------------------------------------------------------------
 
 
 # Evaluate Model ----------------------------------------------------------
-add_fct("predict_test_set", "eval")
-add_fct("collate_predictions", "eval")
-add_fct("calculate_metrics", "eval")
-add_fct("log_metrics", "eval")
+add_function("predict_test_set", "eval")
+add_function("collate_predictions", "eval")
+add_function("calculate_metrics", "eval")
+add_function("log_metrics", "eval")
 # Model Evaluated ---------------------------------------------------------
 
