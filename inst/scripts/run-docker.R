b1c4b1d35f558d6bff37be273834ff81acf56395
harell <harel@loostiger.com>
2020-08-12 13:39:33 +1200

    r/flow

--- inst/scripts/run-docker.R
+++ inst/scripts/run-docker.R
@@ -1,7 +1,7 @@
 source("./R/utils_Docker.R")
 source("./R/utils_DockerCompose.R")
-Docker$new()$kill_all_containers()$remove_dangling_images()
+Docker$new()$kill_all_containers()#$remove_dangling_images()
 
 docker <- DockerCompose$new()
 docker$restart()
-docker$browse_url("r-flow")
+# docker$browse_url("r-flow")
