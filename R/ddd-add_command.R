876034d1acc0f53272e424f91151661e05122843
harell <harel@loostiger.com>
2020-08-13 21:52:59 +1200

    refactor add_command

--- R/ddd-add_command.R
+++ R/ddd-add_command.R
@@ -3,7 +3,57 @@
 #' @param module (`character`) Command module.
 #' @family DDD
 #' @export
-add_command <- function(name, module) {
+add_command <- function(name, module){
+    is.not.null <- Negate(is.null)
+    `%||%` <- function(a,b) if(is.null(a)) b else a
+    dir.create <- function(path) base::dir.create(path, recursive = TRUE, showWarnings = FALSE)
+
+    glue <- stringr::str_glue
+    slug <- paste0("fct_", name)
+    slug <- if(is.not.null(module)) paste0("mod_", module, "_", slug)
+
+    # Write Function ----------------------------------------------------------
+    dir.create(usethis::proj_path("R"))
+    writeLines(
+        glue("
+        #' @title What the Function Does
+        #' @description `{fct_name}` is an amazing function
+        #' @param self (`environment`) A shared environment.
+        #' @return self
+        #' @family {module}
+        #' @export
+        {fct_name} <- function(self) {{
+            # Assertions ...
+            stopifnot(is.environment(self))
+
+            # Code ...
+            self$month <- '{month}'
+
+            # Return
+            invisible(self)
+        }}", fct_name = name, module = module %||% "", month = sample(month.abb, 1)),
+        usethis::proj_path("R", slug, ext = "R")
+    )
+
+    # Write Test --------------------------------------------------------------
+    dir.create(usethis::proj_path("tests", "testthat"))
+    writeLines(
+        glue("
+        context('unit test for {fct_name}')
+
+        # Setup -------------------------------------------------------------------
+        testthat::setup({{
+            assign('test_env', testthat::test_env(), envir = parent.frame())
+            test_env$self <- new.env()
+        }})
+
+        # General -----------------------------------------------------------------
+        test_that('{fct_name} works', {{
+            expect_silent({fct_name}(test_env$self))
+        }})
+        ", fct_name = name),
+        usethis::proj_path("tests", "testthat", paste0("test-", name), ext = "R")
+    )
 
     invisible()
 }
--- tests/testthat/test-add_command.R
+++ tests/testthat/test-add_command.R
@@ -4,13 +4,14 @@ context("unit test for add_command")
 testthat::setup({
     assign("test_env", testthat::test_env(), envir = parent.frame())
     withr::local_dir(tempdir(), .local_envir = test_env)
-    file.create(".here")
+    invisible(file.create(".here"))
 })
 
 # Create R script ---------------------------------------------------------
-test_that("add_command creates new R script", {
+test_that("given name and module, then add_command creates a new R script", {
     name <- "bilbo"
     module <- "baggins"
     path <- usethis::proj_path("R", paste("mod", module, "fct", name, sep = "_"), ext = "R")
     expect_silent(add_command(name, module))
+    expect_file_exists(path)
 })
