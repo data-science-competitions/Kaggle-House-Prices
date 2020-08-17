89500173f330ae85001a4be84c4275666fe52b7b
harell <harel@loostiger.com>
2020-08-17 21:15:34 +1200

    data tests

--- R/dao-ingest_data.R
+++ R/dao-ingest_data.R
@@ -10,6 +10,17 @@ ingest_data <- function(session) {
     session$con <- DBI::dbConnect(RSQLite::SQLite(), path = ":memory:")
     dplyr::copy_to(session$con, house.prices::test_set, "test_set", overwrite = TRUE)
     dplyr::copy_to(session$con, house.prices::train_set, "train_set", overwrite = TRUE)
+    data_tests(session$con)
 
     invisible(session)
+}
+
+data_tests <- function(con){
+    table_names <- c("test_set", "train_set")
+    variable_names <- c("Id", "Neighborhood", "YrSold")
+
+    assertive::assert_is_subset(table_names, DBI::dbListTables(con))
+    for(table_name in table_names) assertive::assert_is_subset(variable_names, DBI::dbListFields(con, table_name))
+
+    invisible()
 }
