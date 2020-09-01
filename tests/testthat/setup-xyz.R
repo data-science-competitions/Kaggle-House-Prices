7c44cfae0fce5da71a1565a5661610f867e0da64
harell <harel@loostiger.com>
2020-09-02 09:39:25 +1200

    instantiate repo during tests

--- tests/testthat/setup-xyz.R
+++ tests/testthat/setup-xyz.R
@@ -1,5 +1,6 @@
-session <- new.env()
-
 # Instantiate Fake Repository ---------------------------------------------
-
+session <- new.env()
+session$repo <- R6DS::RDict$new()
+session$repo$add("historical_data", house.prices::train_set)
+session$repo$add("new_data", house.prices::test_set)
 
