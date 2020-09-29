c35663d9544582a85e2f9fe9213896702d32d94e
harell <harel@loostiger.com>
2020-09-30 09:16:00 +1300

    add event class and derivative

---
+++ R/events.R
@@ -0,0 +1,30 @@
+# nocov start
+#' @title Domain Event
+Event <- R6::R6Class(
+    "Event", lock_objects = FALSE,
+    public = list(
+        #' @description Add key-value pair to event object
+        #' @param key (`character`) Element name.
+        #' @param value (`?`) Element value.
+        set = function(key, value) {self[[key]] <- value},
+        #' @description Initialize new event
+        #' @param ... key-value pairs, e.g. first_name = "Bilbo".
+        initialize = function(...) {
+            args <- unlist(list(...))
+            for(i in seq_len(length(args))){
+                do.call(self$set, args = list(key = names(args[i]), value = unname(args[i])))
+            }
+        }
+    )
+)
+
+
+# Data Pipeline Events ----------------------------------------------------
+#' @title Fail to establish database connection
+#' @param status_code (`integer`) HTTP status codes.
+#' @references \href{https://en.wikipedia.org/wiki/List_of_HTTP_status_codes}{List of HTTP status codes}
+FailedToEstablishConnection <- function(status_code = integer(0)) Event$new(status_code = status_code)
+
+
+# -------------------------------------------------------------------------
+# nocov end
