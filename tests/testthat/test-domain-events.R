d97e383e1108e6e71e97d95758de073f8681bd0e
harell <harel@loostiger.com>
2020-10-01 09:30:54 +1300

    fix test

--- tests/testthat/test-domain-events.R
+++ tests/testthat/test-domain-events.R
@@ -1,3 +1,9 @@
-test_that("FailedToEstablishConnection works", {
-  expect_is(FailedToEstablishConnection(403), "Event")
+test_that("Event class exists", {
+  expect_is(Event$new(), "Event")
 })
+
+test_that("events objects exists", {
+    expect_is(events, "environment")
+    expect_gt(length(ls(events)), 0)
+})
+
