bfb0e2561aa45bf31fc73e671e9edf944613de36
harell <harel@loostiger.com>
2020-10-06 09:20:36 +1300

    log info

--- R/domain-events.R
+++ R/domain-events.R
@@ -1,15 +1,35 @@
+# Domain Events -----------------------------------------------------------
+#' @title Domain Event Class
+#' @description Event parent class that can store common attributes.
+#' @export
+Event <- R6::R6Class(
+    "Event", lock_objects = FALSE,
+    public = list(
+        #' @description Add key-value pair to event object
+        #' @param key (`character`) Element name.
+        #' @param value (`?`) Element value.
+        set = function(key, value) {self[[key]] <- value},
+        #' @description Initialize new event
+        #' @param ... key-value pairs, e.g. first_name = "Bilbo".
+        initialize = function(...) {
+            args <- unlist(list(...))
+            for(i in seq_len(length(args))){
+                self$timestamp <- Sys.time()
+                do.call(self$set, args = list(key = names(args[i]), value = unname(args[i])))
+            }
+        }
+    )
+)
+
+# Domain Events Depository ------------------------------------------------
 #' @title Domain Events Depository
 #' @noRd
 #' @export
 events <- new.env()
 
-
-# Data Pipeline Events ----------------------------------------------------
 events$FailedToEstablishConnection <- function(status_code = integer(0))
     Event$new(event = "FailedToEstablishConnection", status_code = status_code)
 
-
-# Train Model Events ------------------------------------------------------
 events$ObjectCreated <- function(class = char(0))
     Event$new(event = "ObjectCreated", class = class)
 
--- R/service_layer-messagebus.R
+++ R/service_layer-messagebus.R
@@ -1,9 +1,17 @@
+# Subscribe Handlers ------------------------------------------------------
+handlers <- R6DS::RDict$new()
+handlers$add("FailedToEstablishConnection", "log_failure_notification")
+handlers$add("ObjectCreated", "log_created_object")
+
+
 # Define Handlers ---------------------------------------------------------
 log_failure_notification <- function(event){
     msg <- stringr::str_glue("Failed to establish database connection. Server returned status code {code}", code = event$status_code)
     logger::log_error(msg)
 }
 
-# Subscribe Handlers ------------------------------------------------------
-handlers <- R6DS::RDict$new()
-handlers$add("FailedToEstablishConnection", "log_failure_notification")
+log_created_object <- function(event){
+    msg <- stringr::str_glue("{class} object created", class = event$class)
+    logger::log_info(msg)
+}
+
