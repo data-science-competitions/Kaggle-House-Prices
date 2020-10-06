76848ae22826db5e080eb6f3c008f26845df3a6d
harell <harel@loostiger.com>
2020-10-07 09:50:52 +1300

    lgr

--- R/service_layer-messagebus.R
+++ R/service_layer-messagebus.R
@@ -1,3 +1,6 @@
+# <https://s-fleck.github.io/lgr/>
+lgr <- lgr::lgr
+
 # Subscribe Handlers ------------------------------------------------------
 handlers <- R6DS::RDict$new()
 handlers$add("FailedToEstablishConnection", "log_failure_notification")
@@ -7,11 +10,11 @@ handlers$add("ObjectCreated", "log_created_object")
 # Define Handlers ---------------------------------------------------------
 log_failure_notification <- function(event){
     msg <- stringr::str_glue("Failed to establish database connection. Server returned status code {code}", code = event$status_code)
-    logger::log_error(msg)
+    lgr$error(msg)
 }
 
 log_created_object <- function(event){
-    msg <- stringr::str_glue("{class} object created", class = event$class)
-    logger::log_info(msg)
+    msg <- stringr::str_glue("{class} object created on {timestamp}", class = event$class, timestamp = event$timestamp)
+    lgr$info(msg)
 }
 
